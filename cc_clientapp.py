a1ffb2ef56ba95d4a3cb9155e6d7cce2be6efae1
roysangram <42006181+roysangram@users.noreply.github.com>
2018-08-01 22:18:15 +0530

    Add files via upload

---
+++ cc_clientapp.py
@@ -0,0 +1,117 @@
+'''
+Created on 19-Oct-2017
+
+@author: skroy3
+
+     message contains list of nos, process to performed and key_name to the S3 object(file name)
+     puts message in sqs inbox queue
+     puts the same message in S3 bucket
+     waits for response for sqs outbox queue
+     get the message from sqs out box and delete the message from outbox
+     print the message from S3 bucket
+'''
+# Import the SDK
+
+import boto3;
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+#creating sqs client
+sqs = boto3.client('sqs')
+
+#creating s3 client
+s3 = boto3.client('s3')
+
+list_of_nos=[5,6,7]
+bucket_name = "2017ht66117"
+key_name="S3_66117.txt"
+
+#message to sent to S3 bucket and inbox queue
+message_to_be_sent= {"the list of nos":list_of_nos,"key_name":key_name,"operation":"sum//average/product"}
+
+#function to create file to uploaded to S3
+def create_file(message_to_be_sent):
+    file=open(key_name,"a")
+    file.write('\n'+str(message_to_be_sent))
+    file.close()
+    print file.name
+    return file.name
+
+# this function will put the message to inbox queue
+def put_sqs_message(message_to_be_sent):
+    response = sqs.get_queue_url(QueueName='inbox')
+    logging.info("get queue url response info: %s", response)
+    url1 = response['QueueUrl']
+    response =sqs.send_message(
+            QueueUrl=url1,MessageBody=(str(message_to_be_sent))
+     )
+    logging.info("get send message response info: %s", response)
+
+#this function upload the message to S3 bucket
+def add_msg_s3_inbox():
+    s3 = boto3.client('s3')
+    #s3.create_bucket(Bucket='my-bucket')
+    file_name=create_file(message_to_be_sent)
+
+    print "uploading file in to S3 bucket:", file_name
+
+    s3.upload_file(file_name,bucket_name,file_name)
+
+
+# get the response from outbox queue and delete the outbox message
+def get_sqs_message_outbox():
+    response = sqs.get_queue_url(QueueName='outbox')
+    logging.info("get queue url response info: %s", response)
+    url2= response['QueueUrl']
+    response = sqs.receive_message(
+        QueueUrl=url2,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=30,
+        WaitTimeSeconds=20
+    )
+    logging.info("get outbox sqs message response info: %s", response)
+    message = response['Messages'][0]['Body']
+    receipt_handle = response['Messages'][0]['ReceiptHandle']
+    print  "processed message from outbox:",message
+
+    # deleting message
+    response=sqs.delete_message(
+        QueueUrl=url2,
+        ReceiptHandle=receipt_handle)
+    logging.info("deleting sqs message outbox: %s", response)
+
+    return message
+
+#print the S3 bucket details after processing in the console
+def get_s3_message():
+    s3 = boto3.resource('s3')
+
+    print " printing S3 object details after processing: "
+
+    print s3.Object(bucket_name, key_name).get()['Body'].read()
+
+
+if __name__ == "__main__":
+
+    put_sqs_message(message_to_be_sent)
+
+    add_msg_s3_inbox()
+
+    message=get_sqs_message_outbox()
+
+    get_s3_message()
+
+
+
+
+
+
+
---
+++ cc_ec2_instance_app.py
@@ -0,0 +1,131 @@
+'''
+Created on 19-Oct-2017
+
+@author: skroy3
+
+Steps -:
+1- get the sqs message to be processed from inbox queue
+2- delete the message form inbox
+3- process the message and return the result
+4- update the result in S3 bucket
+5- update the result in outbox queue
+
+'''
+# Import the SDK
+import boto3
+import uuid
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# Get the service resource
+sqs = boto3.client('sqs')
+s3 = boto3.client('s3')
+bucket_name="2017ht66117"
+
+
+# create a file to be uploaded into S3 bucket
+def create_file(key_name,message_to_be_sent):
+    file=open(key_name,"a")
+    file.write('\n'+str(message_to_be_sent))
+    file.close()
+    print file.name
+    return file.name
+
+
+# Receive message from inbox SQS queue
+def get_sqs_message():
+    response = sqs.get_queue_url(QueueName='inbox')
+    print response
+    logging.info("get queue url response info: %s", response)
+    url1 = response['QueueUrl']
+    response = sqs.receive_message(
+        QueueUrl=url1,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=30,
+        WaitTimeSeconds=20
+    )
+    logging.info("receive message response info: %s", response)
+    message = response['Messages'][0]['Body']
+    receipt_handle = response['Messages'][0]['ReceiptHandle']
+
+    print "message received from inbox queue: %s",message
+    # deleting  sqs message
+    response=sqs.delete_message(
+        QueueUrl=url1,
+        ReceiptHandle=receipt_handle)
+    logging.info("deleting sqs message inbox: %s", response)
+
+    return message
+
+
+
+#function to process the list of nos
+'''it will take list of three nos and return /
+          some,product and average  in a list '''
+
+
+def process_message( message):
+
+    msg_list=eval(message)
+
+    list_of_nos =msg_list['the list of nos']
+    print "list of no to be processed:",list_of_nos
+    key_name=msg_list['key_name']
+    print "S3 object key/pointer name is :",key_name
+    operation = msg_list['operation']
+    print "operations to be performed:",operation
+    num1 = list_of_nos[0]
+    num2 = list_of_nos[1]
+    num3 = list_of_nos[2]
+    sum = num1+num2+num3
+    average = sum/3
+    product = num1*num2*num3
+
+    result = [sum,average,product]
+
+    return result,key_name,operation
+
+
+# below function is to send message to  sqs outbox after processing
+def send_sqs_message(msg):
+
+    response = sqs.get_queue_url(QueueName='outbox')
+    logging.info("get queue url response info: %s", response)
+    url2 = response['QueueUrl']
+
+    response = sqs.send_message(QueueUrl=url2,\
+                           MessageBody=str(msg))
+    logging.info("send_message response info: %s", response)
+
+
+# function to update s3 bucket with list of nos
+def add_msg_s3_outbox(key_name,message):
+
+    file_name = create_file(key_name,message)
+
+    response=s3.upload_file(file_name, bucket_name, file_name)
+
+    logging.info("response uploaded file in to S3: %s", response)
+
+
+if __name__ == "__main__":
+
+    msg = get_sqs_message()
+
+    pr_msg,key_name,operation = process_message(msg)
+
+    result_message={"the processed list of nos":pr_msg,"key_name":key_name,"operation":operation}
+
+    send_sqs_message(result_message)
+
+    add_msg_s3_outbox(key_name,result_message)
+
+
